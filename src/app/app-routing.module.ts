2b98d6acd3878e59bcdd2dbfe3ab954437924336
sharmilaprajapati <sharmila.prajapati@acando.no>
2020-08-16 15:20:14 +0200

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RecipeComponent} from './components/recipe/recipe.component';
+import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/recipe', pathMatch: 'full' },
+  { path: 'recipe', component: RecipeComponent},
+  { path: 'recipe/:id', component: RecipeComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
